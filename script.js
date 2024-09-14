4335c4c4f48ce125e9ab7d2e96633c018ca29e61
themadseventeen <55276223+themadseventeen@users.noreply.github.com>
2024-09-14 11:46:57 +0200

    ui done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getComputerChoice() {
     const rand = Math.random();
     if (rand <= 0.3333) return "rock";
     if (rand <= 0.6666) return "paper";
-                        return "scissors";
+    return "scissors";
 }
 
 function getHumanChoice() {
@@ -26,25 +26,46 @@ function score(h_choice, c_choice) {
     if (h_choice == "rock" && c_choice == "scissors") return 1;
     if (h_choice == "scissors" && c_choice == "rock") return -1;
     if (h_choice == "scissors" && c_choice == "paper") return 1;
-    
+
 }
 
-function main() {
-    let score_h = 0 , score_c = 0;
-    let round = 0;
+let score_h = 0, score_c = 0;
+let round = 0;
 
-    while (round < 5) {
-        const h_choice = getHumanChoice();
-        const c_choice = getComputerChoice();
-        const eval = score(h_choice, c_choice);
-        
-        if (eval == 1) score_h++;
-        if (eval == -1) score_c++;
-        round++;
+function play(humanChoice) {
+    const h_choice = humanChoice;
+    const c_choice = getComputerChoice();
+    const eval = score(h_choice, c_choice);
+
+    if (eval == 1) score_h++;
+    if (eval == -1) score_c++;
+
+    let element_hscore = document.querySelector(".hscore span");
+    let element_cscore = document.querySelector(".cscore span");
+
+    element_hscore.textContent = score_h;
+    element_cscore.textContent = score_c;
+
+    if (score_c >= 5 || score_h >= 5) {
+        let message_box = document.querySelector(".message");
+        if (score_h == score_c) message_box.textContent = "Tie!";
+        if (score_h > score_c) message_box.textContent = "You win!";
+        else message_box.textContent = "You lose!";
+        const buttons = document.querySelectorAll("button");
+
+        buttons.forEach((button) => {
+            button.disabled = true;
+        });
     }
-    if (score_h == score_c) console.log("Tie!");
-    if (score_h > score_c)  console.log("You win!");
-    else console.log("You lose!");
+
 }
 
-main()
+// main()
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        play(button.textContent.toLowerCase());
+    });
+});
